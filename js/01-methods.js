c42926f6263fb822c05575245d5a44785cf2cc6a
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-04-10 21:30:45 +0300

    update

--- js/01-methods.js
+++ js/01-methods.js
@@ -1,12 +1,85 @@
 /**
  * Методи рядків
  *
+ *  - indexOf
  *  - slice
  *  - toLowerCase/toUpperCase
  *  - includes
  *  - startsWith/endsWith
- *  - indexOf
  *  - trim
  */
+//!=========================================
+// const username = 'Jacob Mercer';
+// const spaceIndex = username.indexOf(' ');
+// console.log(spaceIndex);
+//!=========================================
 
-const username = "Jacob Mercer";
+// const username = 'Jacob Mercer';
+// const index = username.indexOf(' '); //5
+// const firstName = username.slice(0, index);
+// const lastName = username.slice(index + 1);
+// console.log(firstName);
+// console.log(lastName);
+
+// const email = 'volodka@gmail.com';
+// const index = email.indexOf('@');
+// const nickname = email.slice(0, index);
+// console.log(nickname);
+//!=========================================
+// const userMail = 'TestUser@gmail.com';
+// const mail = userMail.toLowerCase();
+// console.log(mail);
+
+//!=========================================
+
+// const userMessage = 'Hello my friend';
+// const message = userMessage.toUpperCase();
+
+// console.log(message);
+
+//!=========================================
+
+// const banWord = 'spam';
+// const userWord = 'Spam';
+// console.log(banWord === userWord);
+
+//!=========================================
+
+// const userMail = 'testuser@mail.ru';
+
+// if (userMail.includes('.ru')) {
+//   console.log('Вийди звідси розбійник');
+// } else {
+//   console.log('Ласкаво просимо');
+// }
+
+// if (!userMail.includes('@')) {
+//   console.log('У вас не валідний емейл');
+// }
+
+//!=========================================
+
+// const userMail = 'testuser@mail.ru';
+// if (userMail.endsWith('.ru')) {
+//   console.log('Вийди звідси розбійник');
+// } else {
+//   console.log('Ласкаво просимо');
+// }
+
+//!=========================================
+
+// const nickname = 'vasya';
+
+// if (!nickname.startsWith('@')) {
+//   console.log('Треба додати @');
+// }
+
+//!=========================================
+
+// const password = '   q.  w.  e rt    ty  ';
+// const result = password.trim();
+
+// console.log(password);
+// console.log(result);
+
+//!=========================================
--- js/03-loops.js
+++ js/03-loops.js
@@ -9,33 +9,120 @@
  * - Цикли while і do...while
  */
 
-console.log("Before");
+//!=========================================
 
-console.log("After");
+// for (let i = 0; i < 100; i += 1) {
+//   console.log('Hello');
+// }
 
-/**
- * -------------------------------
- */
+//!=========================================
 
-console.log("Before");
+// const str = 'Hello world';
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
 
-console.log("After");
+//!=========================================
 
-/**
- * -------------------------------
- */
+// let x = 25;
+// console.log(x);
 
-let a = 10;
+//!=========================================
+// let i = 25;
+// const res = 50 + i;
+// console.log(res);
+
+// for (let i = 50; i > 0; --i) {
+//   console.log(i);
+// }
+
+//!=========================================
+
+// let a = 10;
 
 // console.log(a);
 // console.log(b);
 
-/**
- * -------------------------------
- */
+//!=========================================
 
-console.log("Before while loop");
+//!=========================================
 
-let counter = 0;
+// console.log('Before while loop');
 
-console.log("After while loop");
+// let counter = 0;
+
+// while (counter < 100) {
+//   counter = Number(prompt('Введіть інше число'));
+// }
+
+// console.log('Відправимо завтра', counter);
+
+// console.log('After while loop');
+
+//!=========================================
+
+// const random = Math.round(Math.random() * 100);
+// let counter;
+
+// while (counter !== random) {
+//   if (counter > random) {
+//     console.log('<');
+//   }
+
+//   if (counter < random) {
+//     console.log('>');
+//   }
+
+//   counter = Number(prompt('Enter Value'));
+// }
+
+// console.log('Random value', random);
+// console.log('The End');
+
+//!=========================================
+
+// const theBestSalary = 5000;
+// let salary = 1000;
+
+// for (let i = 0; i < 3; i++) {
+//   console.log('Треба більше зп');
+//   salary += Math.round(Math.random() * 2000);
+//   console.log(`Тепер твоя зп - ${salary}`);
+// }
+
+// console.log(`Привітайте. Маю зп - ${salary}`);
+
+//!=========================================
+
+// const theBestSalary = 2000;
+// let salary = 3000;
+
+// while (salary < theBestSalary) {
+//   console.log('\n\n\nТреба більше зп');
+//   salary += Math.round(Math.random() * 2000);
+//   console.log(`Тепер твоя зп - ${salary}`);
+// }
+
+// console.log(`Привітайте. Маю зп - ${salary}`);
+
+//!=========================================
+
+// const theBestSalary = 2000;
+// let salary = 3000;
+
+// do {
+//   console.log('\n\n\nТреба більше зп');
+//   salary += Math.round(Math.random() * 2000);
+//   console.log(`Тепер твоя зп - ${salary}`);
+// } while (salary < theBestSalary);
+
+// console.log(`Привітайте. Маю зп - ${salary}`);
+
+//!=========================================
+
+// for (let i = 1; i < 1000; i++) {
+//   if (i % 7 === 0 && i % 13 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
