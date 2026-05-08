1a2cb42d0813ebd6b23ad9862496e65f6cc12ecf
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-05-08 21:17:24 +0300

    update

--- js/01-class.js
+++ js/01-class.js
@@ -8,3 +8,231 @@
  *  - Геттери і сеттери
  *  - Статичні властивості
  */
+
+//!=========================================
+
+// const user1 = {
+//   firstName: 'Vasya',
+//   lastName: 'Vasylenko',
+//   age: 10,
+
+//   showName() {
+//     console.log(this.name);
+//   },
+//   showLastname() {
+//     console.log(this.lastName);
+//   },
+//   showAge() {
+//     console.log(this.age);
+//   },
+//   showInfo() {
+//     this.showName();
+//     this.showLastname();
+//     this.showInfo();
+//   },
+// };
+
+// const user1 = {
+//   firstName: 'Vasya',
+//   lastName: 'Vasylenko',
+//   age: 10,
+
+//   showName() {
+//     console.log(this.name);
+//   },
+//   showLastname() {
+//     console.log(this.lastName);
+//   },
+//   showAge() {
+//     console.log(this.age);
+//   },
+//   showInfo() {
+//     this.showName();
+//     this.showLastname();
+//     this.showInfo();
+//   },
+// };
+
+// const user1 = {
+//   firstName: 'Vasya',
+//   lastName: 'Vasylenko',
+//   age: 10,
+
+//   showName() {
+//     console.log(this.name);
+//   },
+//   showLastname() {
+//     console.log(this.lastName);
+//   },
+//   showAge() {
+//     console.log(this.age);
+//   },
+//   showInfo() {
+//     this.showName();
+//     this.showLastname();
+//     this.showInfo();
+//   },
+// };
+
+// const user1 = {
+//   firstName: 'Vasya',
+//   lastName: 'Vasylenko',
+//   age: 10,
+
+//   showName() {
+//     console.log(this.name);
+//   },
+//   showLastname() {
+//     console.log(this.lastName);
+//   },
+//   showAge() {
+//     console.log(this.age);
+//   },
+//   showInfo() {
+//     this.showName();
+//     this.showLastname();
+//     this.showInfo();
+//   },
+// };
+
+//!=========================================
+
+// class User {
+//   constructor(firstnameValue, lastnameValue, ageValue) {
+//     this.firstname = firstnameValue;
+//     this.lastname = lastnameValue;
+//     this.age = ageValue;
+//     this.balance = 0;
+//   }
+
+//   showInfo() {
+//     console.log(this.firstname);
+//     console.log(this.lastname);
+//     console.log(this.balance);
+//     console.log(this.age);
+//   }
+
+//   showFullName() {
+//     console.log(this.firstname, this.lastname);
+//   }
+// }
+
+// const user2 = new User('Roman', 'Romanenko', 21);
+// const user3 = new User('Anton', 'Antonenko', 22);
+// const user4 = new User('Dmytro', 'Noname', 24);
+
+// user2.showInfo();
+
+//!=========================================
+
+// function createUser() {
+//   // this = {};
+//   this.firstname = 'Vasya';
+//   return this;
+// }
+// // const result = createUser.call({});
+// const result = new createUser();
+// console.log(result);
+
+//!=========================================
+
+// class User {
+//   #balance;
+//   #age;
+
+//   constructor(firstname, lastname, age) {
+//     this.fistname = firstname;
+//     this.lastname = lastname;
+//     this.#balance = 0;
+//     this.#age = age;
+//   }
+
+//   showInfo() {
+//     this.#showName();
+//     this.#showLastname();
+//     this.#showBalance();
+//   }
+
+//   #showName() {
+//     console.log(`Firstname ${this.fistname}`);
+//   }
+//   #showLastname() {
+//     console.log(`Lastname ${this.lastname}`);
+//   }
+//   #showBalance() {
+//     console.log(`Balance: ${this.#balance}`);
+//   }
+// }
+
+// const user1 = new User('Roman', 'Vasylenko', 25);
+
+// user1.setAge(30);
+// user1.setAge(-20);
+
+// console.log(user1);
+
+//!=========================================
+
+// class User {
+//   #age;
+
+//   constructor(firstname, lastname, age) {
+//     this.fistname = firstname;
+//     this.lastname = lastname;
+//     this.#age = age;
+//   }
+
+//   get age() {
+//     return `Age: ${this.#age}`;
+//   }
+
+//   set age(newAge) {
+//     if (newAge > 0) {
+//       this.#age = newAge;
+//     }
+//   }
+
+//   setAge() {}
+//   getAge() {}
+// }
+
+// user1;
+
+// const user1 = new User('Vasya', 'Pupkin', 22);
+// user1.age = 30;
+// console.log(user1.age);
+
+// user1.setAge(30);
+// console.log(user1.getAge());
+//!=========================================
+
+// class Dog {
+//   constructor(name, breed) {
+//     this.name = name;
+//     this.breed = breed;
+//   }
+// }
+
+//!=========================================
+
+class Product {
+  static discount = 0.5;
+  static amount = 0;
+
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+    Product.amount += 1;
+  }
+
+  showInfo() {
+    console.log(this.title);
+    console.log(this.price * Product.discount);
+  }
+}
+
+const product1 = new Product('iphone', 1500);
+const product2 = new Product('Apple Watch', 700);
+const product3 = new Product('Apple Watch', 700);
+
+console.log(Product.amount);
--- js/02-inheritance.js
+++ js/02-inheritance.js
@@ -5,3 +5,39 @@
  *  - Конструктор дочірнього класу (super)
  *  - Методи дочірнього класу
  */
+
+class Animal {
+  constructor(breed, age) {
+    this.breed = breed;
+    this.age = age;
+  }
+  eat() {}
+  sleep() {}
+}
+
+class Dog extends Animal {
+  constructor(name, breed, age) {
+    // super(breed, age);
+    // this.name = name;
+    // this.paws = 4;
+  }
+  sayWoof() {}
+}
+
+class Cat extends Animal {
+  constructor() {
+    this.paws = 4;
+  }
+  sayMeow() {}
+}
+
+class Bird extends Animal {
+  constructor() {
+    this.paws = 4;
+  }
+  fly() {}
+}
+
+const dog1 = new Dog('Lord', 'spaniel', 3);
+
+console.log(dog1);
