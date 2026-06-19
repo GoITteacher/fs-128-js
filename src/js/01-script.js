828d5a3534fd0e03c789704e9345620aa63a63cd
Volodymyr Pashchenko <pikimelposhta@gmail.com>
2026-06-19 20:35:53 +0300

    update

--- src/js/01-script.js
+++ src/js/01-script.js
@@ -3,37 +3,37 @@ import { articlesTemplate } from './templates/render-function2';
 
 const refs = {
   form: document.querySelector('.js-search-form'),
-  list: document.querySelector('.js-article-list'),
+  container: document.querySelector('.js-article-list'),
   loadMoreBtn: document.querySelector('.js-btn-load'),
   loader: document.querySelector('.js-loader'),
 };
+
 //!=========================================
-const PER_PAGE = 10;
+
 let query;
 let page;
 let totalPages;
+const PER_PAGE = 8;
 
 //!=========================================
 
 refs.form.addEventListener('submit', async e => {
   e.preventDefault();
-
   const borys = new FormData(e.target);
   query = borys.get('query');
   page = 1;
 
+  hideLoadMoreBtn();
   showLoader();
 
   try {
     const res = await fetchArticles(query, page);
-    totalPages = Math.ceil(res.totalResults / PER_PAGE);
     const markup = articlesTemplate(res.articles);
-    refs.list.innerHTML = markup;
-  } catch {
-    console.log('ERROR');
-  }
+    refs.container.innerHTML = markup;
+    totalPages = Math.ceil(res.totalResults / PER_PAGE);
+  } catch {}
 
-  checkBtnStatus();
+  updateBtnStatus();
   hideLoader();
   e.target.reset();
 });
@@ -42,45 +42,33 @@ refs.form.addEventListener('submit', async e => {
 
 refs.loadMoreBtn.addEventListener('click', async () => {
   page += 1;
-
-  checkBtnStatus();
+  hideLoadMoreBtn();
   showLoader();
-  try {
-    const res = await fetchArticles(query, page);
-    const markup = articlesTemplate(res.articles);
-    refs.list.insertAdjacentHTML('beforeend', markup);
-  } catch {
-    console.log('error');
-  }
-
+  const res = await fetchArticles(query, page);
+  const markup = articlesTemplate(res.articles);
+  refs.container.insertAdjacentHTML('beforeend', markup);
+  updateBtnStatus();
   hideLoader();
 });
 
 //!=========================================
-
-function checkBtnStatus() {
-  if (page < totalPages) {
-    showLoadBtn();
-  } else {
-    hideLoadBtn();
-  }
-}
-
-function showLoadBtn() {
-  refs.loadMoreBtn.disabled = false;
-  // refs.loadMoreBtn.classList.remove('hidden');
-}
-
-function hideLoadBtn() {
-  refs.loadMoreBtn.disabled = true;
-  // refs.loadMoreBtn.classList.add('hidden');
-}
-
-//!=========================================
-
 function showLoader() {
   refs.loader.classList.remove('hidden');
 }
 function hideLoader() {
   refs.loader.classList.add('hidden');
+}
+
+function showLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('hidden');
+}
+function hideLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('hidden');
+}
+function updateBtnStatus() {
+  if (page < totalPages) {
+    showLoadMoreBtn();
+  } else {
+    hideLoadMoreBtn();
+  }
 }
--- src/js/modules/newsAPI.js
+++ src/js/modules/newsAPI.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export const fetchArticles = async (query, page) => {
-  const BASE_URL = 'https://newsapi.org';
-  const END_POINT = '/v2/everything';
-  const url = BASE_URL + END_POINT;
+  const baseUrl = 'https://newsapi.org/v2';
+  const endPoint = '/everything';
+  const url = baseUrl + endPoint;
 
   const params = {
+    page: page,
     q: query,
     apiKey: 'c8747511a2c34730a83caaff4f3693e7',
-    pageSize: 10,
-    page: page,
+    pageSize: 8,
   };
 
   const res = await axios.get(url, { params });
